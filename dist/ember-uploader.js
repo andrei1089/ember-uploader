462b1e06f5501aaa6019a6c49b0c0dc47056b239
Andrei Mirestean <andrei.mirestean@gmail.com>
2015-06-30 17:10:19 +0300

    Rebuild, bump to 0.3.8.1

--- dist/ember-uploader.js
+++ dist/ember-uploader.js
@@ -128,18 +128,27 @@ define("ember-uploader/file-field",
   ["exports"],
   function(__exports__) {
     "use strict";
+    var deprecate = Ember.deprecate;
     var set = Ember.set;
+    var on = Ember.on;
 
-    __exports__["default"] = Ember.TextField.extend({
+    __exports__["default"] = Ember.TextField.extend(Ember.Evented, {
       type: 'file',
       attributeBindings: ['multiple'],
       multiple: false,
       change: function(e) {
         var input = e.target;
         if (!Ember.isEmpty(input.files)) {
-          set(this, 'files', input.files);
+          this.trigger('filesDidChange', input.files);
+          set(this, 'files', input.files); // to be removed in future release, needed for `files` observer to continue working
         }
-      }
+      },
+
+      _deprecateFileObserver: on('init', function() {
+        var hasFilesObserver = this.hasObserverFor('files');
+
+        deprecate('Observing the `files` attr is deprecated, use `filesDidChange` instead.', !hasFilesObserver);
+      })
     });
   });
 define("ember-uploader/s3",
